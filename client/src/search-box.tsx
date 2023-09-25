39a881b4e68294cf9f30e14a4c5469257224513f
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2023-09-24 23:40:10 -0400

    updated custom infinite loading

--- client/src/search-box.tsx
+++ client/src/search-box.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useState, useRef } from 'react';
 import { TextInput, Popover, Box, Stack } from '@mantine/core';
 import { FixedSizeList } from 'react-window';
-import InfiniteLoader from 'react-window-infinite-loader';
 import axios from 'axios';
 
 
@@ -13,17 +12,15 @@ function SearchBox() {
 
     interface Data {
         options: Option[]; 
-        hasMore: boolean; 
+        total: number; 
     }
 
     const [searchText, setSearchText] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState<Data>({
         options: [],
-        hasMore: false,
+        total: 0,
     });
-    const infiniteLoaderRef = useRef<InfiniteLoader | null>(null);
-    // const hasMountedRef = useRef(false);
     const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
     const fetchData = async (searchText: string, limit: number, offset: number) => {
@@ -41,63 +38,49 @@ function SearchBox() {
         }
     };
 
-    const search = async (searchText: string) => {
+    const search = async (searchText: string, limit = 10, offset = 0) => {
         setIsLoading(true);
-        if (searchText === '') {
-            setData({options: [], hasMore: false});
-            setIsLoading(false);
-            return;
-        }
-        const newData = await fetchData(searchText, 10, 0);
+        const newData = await fetchData(searchText, limit, offset);
         if (newData.data) {
-            setData({options: newData.data, hasMore: newData.data.length < newData.total});
+            setData((prevData) => {
+                if (prevData.total === 0) {
+                    prevData.options = [];
+                }
+                return {
+                    options: [...prevData.options, ...newData.data], 
+                    total: newData.total,
+                };
+            });
         } else {
-            setData({options: [{id: 0, name: 'No options found'}], hasMore: false});
+            setData({options: [{id: 0, name: 'No options found'}], total: 0});
         }
         setIsLoading(false);
     };
 
-    const loadMore = async () => {
-        setIsLoading(true);
-        const newData = await fetchData(searchText, 10, data.options.length);
-        setData((prevData) => ({
-            options: [...prevData.options, ...newData.data],
-            hasMore: newData.data.length + data.options.length < newData.total,
-        }));
-        setIsLoading(false);
-    };
 
-    const loadedOptionCount = data.hasMore ? data.options.length + 1 : data.options.length;
-    const loadMoreOptions = (isLoading || !data.hasMore) ? () => {} : loadMore;
-    const isOptionLoaded = useCallback(
-        (index: number) => index < data.options.length,
-        [data]
-    );
+    const handleItemsRendered = ({ visibleStopIndex }: { visibleStopIndex: number }) => {
+        if (data.options.length < data.total && !isLoading && data.options.length === visibleStopIndex + 1) {
+            search(searchText, 10, data.options.length);
+        }
+    }
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchText(e.target.value);
         if (timeoutRef.current) {
             clearTimeout(timeoutRef.current);
         }
-        
-        timeoutRef.current = setTimeout(() => {
-            if (infiniteLoaderRef.current) {
-                infiniteLoaderRef.current.resetloadMoreItemsCache();
-            }
-            search(e.target.value);
-        }, 500);
+
+        setData({ options: [], total: 0 });
+        if (e.target.value !== '') {
+            timeoutRef.current = setTimeout(() => {
+                search(e.target.value);
+            }, 500);
+        }
     };
 
 
-    // useEffect(() => {
-    //     if (hasMountedRef.current) {
-    //         if (infiniteLoaderRef.current) {
-    //             infiniteLoaderRef.current.resetloadMoreItemsCache();
-    //         }
-    //     }
-    //     hasMountedRef.current = true;
-    //     timeoutRef.current = setTimeout(() => search(searchText), 500);
-    // }, [searchText]);
+    const itemCount = isLoading ? data.options.length + 1 : data.options.length;
+    const itemData = isLoading ? [...data.options, {id: 0, name: 'Loading...'}] : data.options;
 
 
     return (
@@ -124,33 +107,28 @@ function SearchBox() {
                 </Popover.Target>
 
                 <Popover.Dropdown>
-                    <InfiniteLoader
-                        ref={infiniteLoaderRef}
-                        isItemLoaded={isOptionLoaded}
-                        itemCount={loadedOptionCount}
-                        loadMoreItems={loadMoreOptions}
-                    >
-                        {({ onItemsRendered, ref }) => (
                             <FixedSizeList
-                                onItemsRendered={onItemsRendered}
-                                ref={ref}
+                                onItemsRendered={handleItemsRendered}
                                 style={{
                                     textAlign: 'center',
                                     fontSize: '0.875rem',
                                 }}
                                 height={300}
                                 width={300}
-                                itemCount={data.options.length}
+                                itemCount={itemCount}
                                 itemSize={45}
+                                itemData={itemData}
                             >
-                                {({ index, style }) => (
+                                {({ index, style, data }) => (
                                     <div style={style}>
-                                        {data.options[index].name}
+                                        {data[index].name === 'Loading...' ? (
+                                            <span>Loading...</span>
+                                        ) : (
+                                            data[index].name
+                                        )}
                                     </div>
                                 )}
                             </FixedSizeList>
-                        )}
-                    </InfiniteLoader>
                 </Popover.Dropdown>
             </Popover>
         </Stack>
